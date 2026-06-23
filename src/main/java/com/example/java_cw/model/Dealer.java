72bd6f96be37c8486d7a6712ba742d1f30bc2bac
Hesandu Ruwanpathirana <hesandurr@users.noreply.github.com>
2026-06-23 22:19:58 +0530

    Add Dealer model with id, name, phone, and location fields

--- src/main/java/com/example/java_cw/model/Dealer.java
+++ src/main/java/com/example/java_cw/model/Dealer.java
@@ -1,4 +1,21 @@
 package com.example.java_cw.model;
 
 public class Dealer {
+
+    public String dealerId;
+    public String dealerName;
+    public String dealerPhone;
+    public String dealerLocation;
+
+    public Dealer(String dealerId, String dealerName, String dealerPhone, String dealerLocation) {
+        this.dealerId = dealerId;
+        this.dealerName = dealerName;
+        this.dealerPhone = dealerPhone;
+        this.dealerLocation = dealerLocation;
+
+    }
+
+    public String toString() {
+        return dealerId + " | " + dealerName + " | " + dealerPhone + " | " + dealerLocation;
+    }
 }
