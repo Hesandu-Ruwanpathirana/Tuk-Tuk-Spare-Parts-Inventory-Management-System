7614dc67d14791e2a7960c470615d404ed7af6e7
Hesandu Ruwanpathirana <hesandurr@users.noreply.github.com>
2026-06-23 22:22:17 +0530

    Add Part model with id, name, brand, price, quantity, category, date, and image fields

--- src/main/java/com/example/java_cw/model/Part.java
+++ src/main/java/com/example/java_cw/model/Part.java
@@ -1,4 +1,28 @@
 package com.example.java_cw.model;
 
 public class Part {
+
+    public String partId;
+    public String partName;
+    public String brand;
+    public double price;
+    public int quantity;
+    public String category;
+    public String dateAdded;
+    public String imagePath;
+
+
+    public Part(String partId, String partName, String brand, double price, int quantity, String category, String dateAdded, String imagePath) {
+        this.partId = partId;
+        this.partName = partName;
+        this.brand = brand;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+        this.dateAdded = dateAdded;
+        this.imagePath = imagePath;
+    }
+    public String toString() {
+        return partId + " | " + partName + " | " + brand + " | Rs." + price + " | Qty:" + quantity + " | " + category + " | " + dateAdded + " | " + imagePath;
+    }
 }
