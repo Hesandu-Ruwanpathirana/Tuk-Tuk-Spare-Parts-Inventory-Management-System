0cf4ad8d9774a7bf8eea0fce5a17fbd4a7f95873
Hesandu Ruwanpathirana <hesandurr@users.noreply.github.com>
2026-06-25 22:09:18 +0530

    Add AuditLogger with append-only file logging

--- src/main/java/com/example/java_cw/util/AuditLogger.java
+++ src/main/java/com/example/java_cw/util/AuditLogger.java
@@ -1,4 +1,31 @@
 package com.example.java_cw.util;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class AuditLogger {
+    public static void log(String filePath, String action, String itemCode, String quantity) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(filePath,true));
+
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String timeStamp = now.format(formatter);
+
+            String logEntry = timeStamp + " | " + action + " | " + itemCode + " | " + quantity;
+
+            writer.write(logEntry);
+            writer.newLine();
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println("Error writing to audit log: " + e.getMessage());
+        }
+    }
+
+
 }
