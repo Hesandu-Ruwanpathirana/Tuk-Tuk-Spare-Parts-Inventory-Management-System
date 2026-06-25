5a93e9d1e85e2b084840189478bbeb3fc4fee0db
Hesandu Ruwanpathirana <hesandurr@users.noreply.github.com>
2026-06-25 11:17:29 +0530

    Add CartItem model with subtotal calculation

--- src/main/java/com/example/java_cw/model/CartItem.java
+++ src/main/java/com/example/java_cw/model/CartItem.java
@@ -1,4 +1,18 @@
 package com.example.java_cw.model;
 
 public class CartItem {
+    public Part part;
+    public int quantity;
+
+    public CartItem(Part part, int quantity) {
+        this.part = part;
+        this.quantity = quantity;
+    }
+    public double getSubTotal() {
+        return part.price * quantity;
+
+    }
+    public String toString() {
+        return part.partName + " x" + quantity + " = Rs." + getSubTotal();
+    }
 }
