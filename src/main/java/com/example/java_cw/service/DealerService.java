fea6448678e2a63defef581e28560231d2400019
Hesandu Ruwanpathirana <hesandurr@users.noreply.github.com>
2026-06-29 13:56:37 +0530

    Add DealerService with random dealer selection and location sorting

--- src/main/java/com/example/java_cw/service/DealerService.java
+++ src/main/java/com/example/java_cw/service/DealerService.java
@@ -1,4 +1,67 @@
 package com.example.java_cw.service;
 
+import com.example.java_cw.model.Dealer;
+import com.example.java_cw.parser.DealerParser;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class DealerService {
+    public List<Dealer> dealers;
+    public String filePath;
+
+
+    public DealerService(String filePath) {
+        this.filePath = filePath;
+        this.dealers = new ArrayList<>();
+    }
+    public void loadDealers() {
+        dealers = DealerParser.parseDealers(filePath);
+    }
+
+    public List<Dealer> getRandomDealers() {
+        List<Dealer> selected = new ArrayList<>();
+        List<Integer> usedIndexes = new ArrayList<>();
+        Random random = new Random();
+
+        while (selected.size() < 4) {
+            int index = random.nextInt(dealers.size());
+
+            boolean alreadyUsed = false;
+            for (int i = 0; i < usedIndexes.size(); i++) {
+                if (usedIndexes.get(i) == index) {
+                    alreadyUsed = true;
+                    break;
+
+                }
+            }
+            if(!alreadyUsed) {
+                selected.add(dealers.get(index));
+                usedIndexes.add(index);
+            }
+
+        }
+        return sortByLocation(selected);
+
+    }
+    public List<Dealer> sortByLocation(List<Dealer> dealersToSort) {
+        List<Dealer> sorted = new ArrayList<>(dealersToSort);
+
+        for (int i = 0; i < sorted.size() - 1; i++) {
+            for (int j = 0; j < sorted.size() -i -1; j++) {
+                Dealer a = sorted.get(j);
+                Dealer b = sorted.get(j+1);
+
+                if(a.dealerLocation.toLowerCase().compareTo(b.dealerLocation.toLowerCase()) > 0) {
+                    sorted.set(j,b);
+                    sorted.set(j+1,a);
+                }
+            }
+        }
+        return sorted;
+
+    }
+    public int getTotalDealers() {
+        return dealers.size();
+    }
 }
