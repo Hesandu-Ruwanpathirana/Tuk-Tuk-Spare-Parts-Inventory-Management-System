46496a05f3dea90ea42dae4f7f7553e5da082379
Hesandu Ruwanpathirana <hesandurr@users.noreply.github.com>
2026-07-11 03:48:53 +0530

    Fixed Error in controller class

--- src/main/java/com/example/java_cw/MainController.java
+++ src/main/java/com/example/java_cw/MainController.java
@@ -9,7 +9,6 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
