f6c062384fbea01ae79854c4b5aa465c6e667639
Hesandu Ruwanpathirana <hesandurr@users.noreply.github.com>
2026-07-24 00:50:02 +0530

    Create JUnit test setup for CartItem class

---
+++ src/test/java/com/example/java_cw/model/CartItemTest.java
@@ -0,0 +1,28 @@
+package com.example.java_cw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartItemTest {
+
+    @Test
+    void getPart() {
+    }
+
+    @Test
+    void getQuantity() {
+    }
+
+    @Test
+    void setQuantity() {
+    }
+
+    @Test
+    void getSubTotal() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
