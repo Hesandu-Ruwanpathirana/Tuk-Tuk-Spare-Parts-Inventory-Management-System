f32cc598038fba3c59f88b61753b851f9134f898
Hesandu Ruwanpathirana <hesandurr@users.noreply.github.com>
2026-07-24 00:20:25 +0530

    Create JUnit test setup for Part class

---
+++ src/test/java/com/example/java_cw/model/PartTest.java
@@ -0,0 +1,28 @@
+package com.example.java_cw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PartTest {
+
+    @Test
+    void setPrice() {
+    }
+
+    @Test
+    void setQuantity() {
+    }
+
+    @Test
+    void setCategory() {
+    }
+
+    @Test
+    void setLowStockThreshold() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
