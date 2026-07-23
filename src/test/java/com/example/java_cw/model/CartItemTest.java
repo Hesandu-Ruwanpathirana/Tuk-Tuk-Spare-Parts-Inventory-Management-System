55c08482eac9fa086c3f1e1d0e54d6badfbe7f95
Hesandu Ruwanpathirana <hesandurr@users.noreply.github.com>
2026-07-24 00:58:38 +0530

    Add JUnit test setup for setQuantity method

--- src/test/java/com/example/java_cw/model/CartItemTest.java
+++ src/test/java/com/example/java_cw/model/CartItemTest.java
@@ -32,6 +32,14 @@ class CartItemTest {
 
     @Test
     void setQuantity() {
+        Part part = new Part(
+                "P001","Brake Pad","TVS",
+                1000,10,"brakes",
+                "01-01-2025","",5);
+
+        CartItem item = new CartItem(part,2);
+        item.setQuantity(5);
+        assertEquals(5, item.getQuantity());
     }
 
     @Test
