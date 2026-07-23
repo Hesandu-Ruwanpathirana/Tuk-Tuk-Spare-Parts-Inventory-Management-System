b915c65fd659a02cac68db761caa09c64792d881
Hesandu Ruwanpathirana <hesandurr@users.noreply.github.com>
2026-07-23 20:11:52 +0530

    Create Junit Tests for Cart class

---
+++ src/test/java/com/example/java_cw/model/CartTest.java
@@ -0,0 +1,36 @@
+package com.example.java_cw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CartTest {
+
+    @Test
+    void addItem() {
+    }
+
+    @Test
+    void removeItem() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void getTotal() {
+    }
+
+    @Test
+    void hasCategory() {
+    }
+
+    @Test
+    void checkout() {
+    }
+}
