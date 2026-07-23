ecf11d59f422b7029b1a4fa1c734efb4d9cca35e
Hesandu Ruwanpathirana <hesandurr@users.noreply.github.com>
2026-07-24 00:59:42 +0530

    Add JUnit test setup for setSubTotal method

--- src/test/java/com/example/java_cw/model/CartItemTest.java
+++ src/test/java/com/example/java_cw/model/CartItemTest.java
@@ -44,6 +44,13 @@ class CartItemTest {
 
     @Test
     void getSubTotal() {
+            Part part = new Part(
+                    "P001","Brake Pad","TVS",
+                    1000,10,"brakes",
+                    "01-01-2025","",5);
+
+            CartItem item = new CartItem(part,3);
+            assertEquals(3000, item.getSubTotal());
     }
 
     @Test
