5577d485d626e146ae495d58df5cd0efb076028d
Hesandu Ruwanpathirana <hesandurr@users.noreply.github.com>
2026-07-24 00:38:46 +0530

    Create JUnit test setup for Dealer class

---
+++ src/test/java/com/example/java_cw/model/DealerTest.java
@@ -0,0 +1,28 @@
+package com.example.java_cw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DealerTest {
+
+    @Test
+    void getDealerId() {
+    }
+
+    @Test
+    void getDealerName() {
+    }
+
+    @Test
+    void getDealerPhone() {
+    }
+
+    @Test
+    void getDealerLocation() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
