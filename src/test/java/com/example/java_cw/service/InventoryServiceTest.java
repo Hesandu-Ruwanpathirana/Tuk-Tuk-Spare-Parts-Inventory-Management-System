071bc58881a6e3a5291822020ed55014d35d4d7e
Hesandu Ruwanpathirana <hesandurr@users.noreply.github.com>
2026-07-23 23:10:28 +0530

    Create JUnit test setup for InventoryService class

---
+++ src/test/java/com/example/java_cw/service/InventoryServiceTest.java
@@ -0,0 +1,43 @@
+package com.example.java_cw.service;
+
+import com.example.java_cw.model.Part;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryServiceTest {
+
+    @Test
+    void addPart() {
+    }
+
+    @Test
+    void deletePart() {
+    }
+
+    @Test
+    void updatePart() {
+    }
+
+    @Test
+    void searchParts() {
+    }
+
+    @Test
+    void getLowStockParts() {
+    }
+
+    @Test
+    void sortParts() {
+    }
+
+    @Test
+    void getTotalValue() {
+    }
+
+    @Test
+    void getTotalCount() {
+    }
+}
