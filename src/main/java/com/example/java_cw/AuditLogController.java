fb8a1b03ddbe71f226b58f34310c617029f8e7b4
Hesandu Ruwanpathirana <hesandurr@users.noreply.github.com>
2026-07-28 16:48:22 +0530

    Add AuditLogController with FXML field bindings for audit log table

--- src/main/java/com/example/java_cw/AuditLogController.java
+++ src/main/java/com/example/java_cw/AuditLogController.java
@@ -1,4 +1,24 @@
 package com.example.java_cw;
 
-public class AuditLogController {
+import com.example.java_cw.model.AuditEntry;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AuditLogController implements Initializable {
+    @FXML private TableView<AuditEntry> auditLogTable;
+    @FXML private TableColumn<AuditEntry, String> colLogTimestamp;
+    @FXML private TableColumn<AuditEntry, String> colLogAction;
+    @FXML private TableColumn<AuditEntry, String> colLogItemCode;
+    @FXML private TableColumn<AuditEntry, String> colLogDetails;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
